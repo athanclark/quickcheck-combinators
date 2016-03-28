3288a82570f3262bbcf40f0a5b420fd37b9b93d4
Athan Clark <athan.clark@gmail.com>
2016-03-28 16:30:38 -0600

    more constructive

--- src/Test/QuickCheck/Combinators.hs
+++ src/Test/QuickCheck/Combinators.hs
@@ -11,55 +11,66 @@ module Test.QuickCheck.Combinators where
 import GHC.TypeLits
 import Data.Proxy
 
+import Data.Maybe (fromMaybe)
+import Data.Unfoldable (Unfoldable, fromList)
+import Control.Applicative (Alternative (empty))
+import Control.Monad (replicateM)
+
 import Test.QuickCheck
 
 
+-- * Foldable
+
+-- | Generate with a minimum, inclusive size as @n :: Nat@
 newtype AtLeast (n :: Nat) f x = AtLeast (f x)
   deriving (Show, Read, Eq, Ord, Enum)
 
-instance ( Foldable f
+instance ( Unfoldable t
+         , Alternative t
          , Arbitrary x
-         , Arbitrary (f x)
+         , Arbitrary (t x)
          , KnownNat n
-         ) => Arbitrary (AtLeast (n :: Nat) f x) where
-  arbitrary = do
+         ) => Arbitrary (AtLeast (n :: Nat) t x) where
+  arbitrary = sized $ \m' -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
-    (fs :: f x) <- arbitrary `suchThat`
-                     (\fs' -> length fs' >= n')
-    return (AtLeast fs)
+    k <- choose (n', m')
+    (ts :: t x) <- (fromMaybe empty . fromList) <$> replicateM k arbitrary
+    return (AtLeast ts)
 
-
+-- | Generate with a maximum, inclusive size as @n :: Nat@
 newtype AtMost (n :: Nat) f x = AtMost (f x)
   deriving (Show, Read, Eq, Ord, Enum)
 
-instance ( Foldable f
+instance ( Unfoldable t
+         , Alternative t
          , Arbitrary x
-         , Arbitrary (f x)
-         , KnownNat n
-         ) => Arbitrary (AtMost (n :: Nat) f x) where
-  arbitrary = do
-    let n' = fromIntegral $ natVal (Proxy :: Proxy n)
-    (fs :: f x) <- arbitrary `suchThat`
-                     (\fs' -> length fs' <= n')
-    return (AtMost fs)
+         , Arbitrary (t x)
+         , KnownNat m
+         ) => Arbitrary (AtMost (m :: Nat) t x) where
+  arbitrary = sized $ \m'' -> do
+    let m' = fromIntegral $ natVal (Proxy :: Proxy m)
+    k <- choose (0, min m' m'')
+    (ts :: t x) <- (fromMaybe empty . fromList) <$> replicateM k arbitrary
+    return (AtMost ts)
 
+-- | Generate between the inclusive range of @n :: Nat@ and @m :: Nat@
 newtype Between (n :: Nat) (m :: Nat) f x = Between (f x)
   deriving (Show, Read, Eq, Ord, Enum)
 
-instance ( Foldable f
+instance ( Unfoldable t
+         , Alternative t
          , Arbitrary x
-         , Arbitrary (f x)
+         , Arbitrary (t x)
          , KnownNat n
          , KnownNat m
-         ) => Arbitrary (Between (n :: Nat) (m :: Nat) f x) where
-  arbitrary = do
+         ) => Arbitrary (Between (n :: Nat) (m :: Nat) t x) where
+  arbitrary = sized $ \m'' -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
         m' = fromIntegral $ natVal (Proxy :: Proxy m)
-    (fs :: f x) <- arbitrary `suchThat`
-                     (\fs' -> length fs' >= n'
-                           && length fs' <= m')
-    return (Between fs)
+    k <- choose (n', min m' m'')
+    (ts :: t x) <- (fromMaybe empty . fromList) <$> replicateM k arbitrary
+    return (Between ts)
 
-
+-- | Convenience for @AtLeast 1@
 newtype NonEmpty f x = NonEmpty (AtLeast 1 f x)
   deriving (Show, Read, Eq, Ord, Enum, Arbitrary)
