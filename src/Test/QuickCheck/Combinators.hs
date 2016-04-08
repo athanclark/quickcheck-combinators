33358637f9e2b2c12b317efa590f0cde35ea8109
Athan Clark <athan.clark@gmail.com>
2016-04-08 15:23:08 -0600

    compiles!

--- src/Test/QuickCheck/Combinators.hs
+++ src/Test/QuickCheck/Combinators.hs
@@ -4,6 +4,8 @@
   , FlexibleContexts
   , KindSignatures
   , GeneralizedNewtypeDeriving
+  , ConstraintKinds
+  , UndecidableInstances
   #-}
 
 module Test.QuickCheck.Combinators where
@@ -12,7 +14,7 @@ import GHC.TypeLits
 import Data.Proxy
 
 import Data.Maybe (fromMaybe)
-import Data.Unfoldable (Unfoldable, fromList)
+import Data.Unfoldable.Restricted (UnfoldableR, fromList)
 import Control.Monad (replicateM)
 
 import Test.QuickCheck
@@ -20,15 +22,16 @@ import Test.QuickCheck
 
 
 -- | Generate with a minimum, inclusive size as @n :: Nat@
-newtype AtLeast (n :: Nat) t x = AtLeast (t x)
+newtype AtLeast (n :: Nat) t a = AtLeast (t a)
   deriving (Show, Read, Eq, Ord, Enum)
 
-instance ( Unfoldable t
-         , Monoid (t x)
-         , Arbitrary x
-         , Arbitrary (t x)
+instance ( UnfoldableR p t
+         , Monoid (t a)
+         , Arbitrary a
+         , Arbitrary (t a)
          , KnownNat n
-         ) => Arbitrary (AtLeast (n :: Nat) t x) where
+         , p a
+         ) => Arbitrary (AtLeast (n :: Nat) t a) where
   arbitrary = sized $ \m' -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
     k <- choose (n', m')
@@ -36,15 +39,16 @@ instance ( Unfoldable t
     return (AtLeast ts)
 
 -- | Generate with a maximum, inclusive size as @n :: Nat@
-newtype AtMost (n :: Nat) t x = AtMost (t x)
+newtype AtMost (n :: Nat) t a = AtMost (t a)
   deriving (Show, Read, Eq, Ord, Enum)
 
-instance ( Unfoldable t
-         , Monoid (t x)
-         , Arbitrary x
-         , Arbitrary (t x)
+instance ( UnfoldableR p t
+         , Monoid (t a)
+         , Arbitrary a
+         , Arbitrary (t a)
          , KnownNat m
-         ) => Arbitrary (AtMost (m :: Nat) t x) where
+         , p a
+         ) => Arbitrary (AtMost (m :: Nat) t a) where
   arbitrary = sized $ \m'' -> do
     let m' = fromIntegral $ natVal (Proxy :: Proxy m)
     k <- choose (0, min m' m'')
@@ -52,16 +56,17 @@ instance ( Unfoldable t
     return (AtMost ts)
 
 -- | Generate between the inclusive range of @n :: Nat@ and @m :: Nat@
-newtype Between (n :: Nat) (m :: Nat) t x = Between (t x)
+newtype Between (n :: Nat) (m :: Nat) t a = Between (t a)
   deriving (Show, Read, Eq, Ord, Enum)
 
-instance ( Unfoldable t
-         , Monoid (t x)
-         , Arbitrary x
-         , Arbitrary (t x)
+instance ( UnfoldableR p t
+         , Monoid (t a)
+         , Arbitrary a
+         , Arbitrary (t a)
          , KnownNat n
          , KnownNat m
-         ) => Arbitrary (Between (n :: Nat) (m :: Nat) t x) where
+         , p a
+         ) => Arbitrary (Between (n :: Nat) (m :: Nat) t a) where
   arbitrary = sized $ \m'' -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
         m' = fromIntegral $ natVal (Proxy :: Proxy m)
