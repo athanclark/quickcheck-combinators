c7380de55bf403752031d12f0bae3a4ffb5820c8
Koz Ross <koz.ross@retro-freedom.nz>
2016-06-18 17:08:53 +1200

    Standardizing layout to match originals, removing redundant type annotations

--- src/Test/QuickCheck/Combinators.hs
+++ src/Test/QuickCheck/Combinators.hs
@@ -35,17 +35,18 @@ instance ( UnfoldableR p t
   arbitrary = sized $ \m' -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
     k <- choose (min n' m', max n' m')
-    (ts :: t x) <- (fromMaybe mempty . fromList) <$> replicateM k arbitrary
-    return (AtLeast ts)
+    ts <- (fromMaybe mempty . fromList) <$> replicateM k arbitrary
+    return . AtLeast $ ts
 
-instance ( Arbitrary a,
-           Ord a,
-           KnownNat n) => Arbitrary (AtLeast (n :: Nat) OrderedList a) where
+instance ( Arbitrary a
+         , Ord a
+         , KnownNat n) => Arbitrary (AtLeast (n :: Nat) OrderedList a) where
   arbitrary = sized $ \m -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
+        mkOrd = Ordered . L.sort . fromMaybe mempty . fromList
     k <- choose (min n' m, max n' m)
-    (ts :: OrderedList a) <- (Ordered . L.sort . fromMaybe mempty . fromList) <$> replicateM k arbitrary
-    return (AtLeast ts)
+    ts <- mkOrd <$> replicateM k arbitrary
+    return . AtLeast $ ts
     
 -- | Generate with a maximum, inclusive size as @n :: Nat@
 newtype AtMost (n :: Nat) t a = AtMost (t a)
@@ -60,17 +61,18 @@ instance ( UnfoldableR p t
   arbitrary = sized $ \m'' -> do
     let m' = fromIntegral $ natVal (Proxy :: Proxy m)
     k <- choose (0, min m' m'')
-    (ts :: t x) <- (fromMaybe mempty . fromList) <$> replicateM k arbitrary
-    return (AtMost ts)
+    ts <- (fromMaybe mempty . fromList) <$> replicateM k arbitrary
+    return . AtMost $ ts
 
-instance ( Arbitrary a,
-           Ord a,
-           KnownNat n) => Arbitrary (AtMost (n :: Nat) OrderedList a) where
+instance ( Arbitrary a
+         , Ord a
+         , KnownNat n) => Arbitrary (AtMost (n :: Nat) OrderedList a) where
   arbitrary = sized $ \m -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
+        mkOrd = Ordered . L.sort . fromMaybe mempty . fromList
     k <- choose (0, min n' m)
-    (ts :: OrderedList a) <- (Ordered . L.sort . fromMaybe mempty . fromList) <$> replicateM k arbitrary
-    return (AtMost ts)    
+    ts <- mkOrd <$> replicateM k arbitrary
+    return . AtMost $ ts    
 
 -- | Generate between the inclusive range of @n :: Nat@ and @m :: Nat@
 newtype Between (n :: Nat) (m :: Nat) t a = Between (t a)
@@ -87,19 +89,20 @@ instance ( UnfoldableR p t
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
         m' = fromIntegral $ natVal (Proxy :: Proxy m)
     k <- choose (n', min m' m'')
-    (ts :: t x) <- (fromMaybe mempty . fromList) <$> replicateM k arbitrary
-    return (Between ts)
+    ts <- (fromMaybe mempty . fromList) <$> replicateM k arbitrary
+    return . Between $ ts
 
-instance ( Arbitrary a,
-           Ord a,
-           KnownNat n,
-           KnownNat m) => Arbitrary (Between (n :: Nat) (m :: Nat) OrderedList a) where
+instance ( Arbitrary a
+         , Ord a
+         , KnownNat n
+         , KnownNat m) => Arbitrary (Between (n :: Nat) (m :: Nat) OrderedList a) where
   arbitrary = sized $ \s -> do
     let n' = fromIntegral $ natVal (Proxy :: Proxy n)
         m' = fromIntegral $ natVal (Proxy :: Proxy m)
+        mkOrd = Ordered . L.sort . fromMaybe mempty . fromList
     k <- choose (n', min m' s)
-    (ts :: OrderedList a) <- (Ordered . L.sort . fromMaybe mempty . fromList) <$> replicateM k arbitrary
-    return (Between ts)
+    ts <- mkOrd <$> replicateM k arbitrary
+    return . Between $ ts
 
 -- | Convenience for @AtLeast 1@
 type NonMempty = AtLeast 1
